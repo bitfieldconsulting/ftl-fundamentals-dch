e54cb4c69e850f892f95bdeac1837dbd2e8ca09b
Dave Cottlehuber <dch@skunkwerks.at>
2021-01-05 22:57:59 +0000

    add variadic capability & tests to multiply

--- calculator.go
+++ calculator.go
@@ -19,10 +19,14 @@ func Subtract(a, b float64, extras ...float64) float64 {
 	return a - Add(0, b, extras...)
 }
 
-// Multiply takes two floats and returns the result of multiplying the first
-// by the second.
-func Multiply(a, b float64) float64 {
-	return a * b
+// Multiply takes two or more floats and returns the result of multiplying the first
+// by the remaining parameters.
+func Multiply(a, b float64, extras ...float64) float64 {
+	result := a * b
+	for _, v := range extras {
+		result *= v
+	}
+	return result
 }
 
 // Divide takes two floats and returns the result of dividing the first
